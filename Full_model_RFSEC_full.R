99799a98c1ad60f2a8ba8ec94818a0877a69c2cf
Sunjae Bae <sunjae.bae1@gmail.com>
2024-11-15 14:09:46 -0500

    parallel & speed up

--- Full_model_RFSEC_full.R
+++ Full_model_RFSEC_full.R
@@ -1,5 +1,5 @@
 # Load libraries and source required scripts
-setwd("/gpfs/home/kantap01/wml/Final")
+# setwd("/gpfs/home/kantap01/wml/Final")
 
 source("setup.R")
 
@@ -7,17 +7,28 @@ train_set <- readRDS("train_set.rds")
 test_set <- readRDS("test_set.rds")
 #################### RANDOM FOREST SURVIVAL ################################
 
+# Tell RF to use 10 cores
+ncore <- 10
+options(rf.cores=ncore, mc.cores=ncore)
+
+# Use a 10% random subset for tuning
+samp_prop <- 0.1
+tune_subset <- sample(nrow(train_set), nrow(train_set)*samp_prop)
+train_set_small <- train_set[tune_subset,]
+
 # Perform hyperparameter tuning
+system.time( # Just adding a stopwatch
 tune_results <- tune(
   formula = as.formula(paste("Surv(X_t, X_d) ~", paste(variables_0, collapse = " + "))), 
-  data = train_set,
-  ntreeTry = 20,   # Please change this to 750
+  data = train_set_small,
+  ntreeTry = 750,   # Please change this to 750
   nodesizeTry = c(5, 10, 15, 20, 25, 30, 35),  
   mtryStart = 5,  
   stepFactor = 1.2,  # Smaller step size for finer tuning
   improve = 1e-3,  # Stricter improvement criterion
   maxIter = 30,  # Increase max iterations for a more comprehensive search
   trace = TRUE,
+)
 )
 
 # Extract the best parameters from tuning results
@@ -26,19 +37,27 @@ print("Best Parameters from Tuning:")
 print(best_params)
 
 # Train the final model with the best hyperparameters using parallel processing
+system.time( # Just adding a stopwatch
 rfs_full <- rfsrc(
   formula = as.formula(paste("Surv(X_t, X_d) ~", paste(variables_0, collapse = " + "))),
   data = train_set,
-  ntree = 20,  # Please change this to 750
+  ntree = 750,  # Please change this to 750
   mtry = best_params["mtry"],
   nodesize = best_params["nodesize"],
   importance = TRUE, 
-  na.action = "na.impute"
+  na.action = "na.impute",
+# Let's also try some tricks from https://www.randomforestsrc.org/articles/speedup.html
+  importance="none",
+  ntime=100,
+  perf.type='none' ### This turns of the internal calculation for C-stat. We do this later anyways, correct?
+)
 )
 
 # Save and print the results
 saveRDS(rfs_full, "rfs_full_model_0.rds")
-var_importance <- rfs_full$importance
-top_variables <- sort(var_importance, decreasing = TRUE)
-print("Top Variables by Importance:")
-print(top_variables)
+
+#### I set importance="none" to speed things up. Now we will need to run a prediction on the training set to make this work.
+# var_importance <- rfs_full$importance   
+# top_variables <- sort(var_importance, decreasing = TRUE)
+# print("Top Variables by Importance:")
+# print(top_variables)
